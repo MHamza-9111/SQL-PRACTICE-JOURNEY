b5385a1bdfe6f8f45e0cd446acae6e8868dd3faa
Muhammad_Hamza <mughalhamza2015@gmail.com>
2025-10-02 11:29:55 +0500

    Add files via upload

---
+++ CLASS NO 1/CLASS NO 1.sql
@@ -0,0 +1,19 @@
+-- Class 1 - SQL Basics Practice
+
+-- 1. Create a new database
+CREATE DATABASE Class1_Database;
+
+-- 2. Print your name
+SELECT 'Hamza Mughal' AS Name;
+
+-- 3. Print your age
+SELECT 19 AS Age;
+
+-- 4. Add two numbers
+SELECT 10 + 15 AS Sum;
+
+-- 5. Get today’s date
+SELECT GETDATE() AS TodayDate;
+
+-- 6. Combine name and age in one formatted query
+SELECT 'NAME: Hamza Mughal, AGE: 19' AS Info;
