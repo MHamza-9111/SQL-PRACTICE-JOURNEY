e4106890bdb347fb13b6a2402c781da9422f28b7
Muhammad_Hamza <mughalhamza2015@gmail.com>
2025-10-09 13:26:27 +0500

    CLASS NO 8.sql

--- CLASS NO 8/CLASS NO 8.sql
+++ CLASS NO 8/CLASS NO 8.sql
@@ -1,8 +1,8 @@
 -- CLASS NO 8 TABLE RELATIONS WITH FOREIGN & PRIMARY KEY FUNCTION --
 
-CREATE DATABASE University;
+CREATE DATABASE HMUniversity;
 
-USE University;
+USE HMUniversity;
 
 CREATE TABLE Teachers (
     teacher_id INT IDENTITY (1,1) PRIMARY KEY,
@@ -22,9 +22,11 @@ CREATE TABLE Courses (
 CREATE TABLE Enrollments (
     enrollment_id INT IDENTITY (1,1) PRIMARY KEY,
     student_id INT,
+    teacher_id INT,
     course_id INT,
     enrollment_date DATE,
     FOREIGN KEY (student_id) REFERENCES Students(student_id),
+    FOREIGN KEY (teacher_id) REFERENCES Teachers(teacher_id),
     FOREIGN KEY (course_id) REFERENCES Courses(course_id)
 );
 
@@ -44,18 +46,20 @@ INSERT INTO Courses (course_name) VALUES
 ('Physics'),
 ('Computer Science');
 
-INSERT INTO Enrollments (student_id, course_id, enrollment_date) VALUES
-(1, 1, '2025-08-15'),
-(1, 2, '2025-08-15'),
-(2, 2, '2025-08-16'),
-(3, 3, '2025-08-17'),
-(4, 1, '2025-08-18'),
-(4, 3, '2025-08-18');
+INSERT INTO Enrollments (student_id, teacher_id, course_id, enrollment_date) VALUES
+(1, 2, 1, '2025-08-15'),
+(1, 2, 2, '2025-08-15'),
+(2, 1, 2, '2025-08-16'),
+(3, 3, 3, '2025-08-17'),
+(4, 1, 1, '2025-08-18'),
+(4, 3, 3, '2025-08-18');
 
 SELECT 
-Students.full_name,
+Students.full_name AS student_name,
+Teachers.full_name AS teacher_name,
 Courses.course_name,
 Enrollments.enrollment_date
 FROM Enrollments
 JOIN Students ON Enrollments.student_id = Students.student_id
+JOIN Teachers ON Enrollments.teacher_id = Teachers.teacher_id
 JOIN Courses ON Enrollments.course_id = Courses.course_id;
