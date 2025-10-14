6813469028471fb8d5407f7a5a3c4d3d0f97df82
Muhammad_Hamza <mughalhamza2015@gmail.com>
2025-10-13 22:37:39 -0700

    Add files via upload

---
+++ CLASS NO 9/CLASS 9 ONE to ONE.sql
@@ -0,0 +1,39 @@
+USE HM_School;
+
+CREATE TABLE Users(
+	UserID INT PRIMARY KEY,
+	Name VARCHAR(10) NOT NULL,
+	Email VARCHAR(50) NOT NULL,
+	Phone VARCHAR(30) NULL
+);
+
+CREATE TABLE UserProfiles(
+	UserID INT UNIQUE,
+	ProfileID INT IDENTITY(100,1) UNIQUE,
+	Platform VARCHAR(50)
+	FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+INSERT INTO Users (UserID, Name, Email, Phone)
+VALUES
+(42101, 'Hamza', 'mughalhamza2015@gmail.com', '+923002799288'),
+(99288, 'Shayan', 'shayan911@gmail.com', '+9230021192884'),
+(60501, 'Zaid', 'zaid1080@gmail.com', '+923331218754'),
+(47855, 'Mubashir', 'mubashir378@gmail.com', '+923213889110')
+
+INSERT INTO UserProfiles (UserID, Platform)
+VALUES
+(42101, 'Facebook'),
+(99288, 'Linkedin'),
+(47855, 'Instagram'),
+(60501, 'Shopify')
+
+SELECT
+u.UserID,
+u.Name,
+p.ProfileID,
+p.Platform,
+u.Email,
+u.Phone
+FROM Users u
+JOIN UserProfiles p ON u.UserID = p.UserID
